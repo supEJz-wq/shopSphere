d62340f6f6466e784c8dca8de2581f25c9faeaa7
zup_ejz <erroljaydepaz@gmail.com>
2026-08-19 21:53:50 +0800

    fix: allow sellers and admins to report products

--- server/src/routes/report.routes.js
+++ server/src/routes/report.routes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const reportController = require('../controllers/report.controller');
 const { protect } = require('../middleware/auth.middleware');
-const { authorize } = require('../middleware/authorize.middleware');
 const validate = require('../middleware/validate.middleware');
 const { createReportSchema } = require('../validations/report.validation');
 
 const router = express.Router();
 
-router.post('/', protect, authorize('customer'), validate(createReportSchema), reportController.createReport);
+router.post('/', protect, validate(createReportSchema), reportController.createReport);
 
 module.exports = router;
